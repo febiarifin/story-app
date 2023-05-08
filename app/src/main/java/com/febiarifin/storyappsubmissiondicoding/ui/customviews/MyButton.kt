a7a0d98ec721cc8537afc948819b42f665b1a267
febiarifin <febiarifin0@gmail.com>
2023-05-08 15:48:34 +0700

    implementation revision from submission 1

--- app/src/main/java/com/febiarifin/storyappsubmissiondicoding/ui/customviews/MyButton.kt
+++ app/src/main/java/com/febiarifin/storyappsubmissiondicoding/ui/customviews/MyButton.kt
@@ -4,15 +4,13 @@ import android.content.Context
 import android.graphics.drawable.Drawable
 import android.util.AttributeSet
 import android.view.Gravity
+import androidx.appcompat.content.res.AppCompatResources
 import androidx.appcompat.widget.AppCompatButton
 import androidx.core.content.ContextCompat
 import com.febiarifin.storyappsubmissiondicoding.R
 
 class MyButton: AppCompatButton {
-    private lateinit var enabledBackground: Drawable
-    private lateinit var disabledBackground: Drawable
     private var txtColor: Int = 0
-    private var buttonTitle: String = ""
 
     constructor(context: Context) : super(context) {
         init()
@@ -35,14 +33,7 @@ class MyButton: AppCompatButton {
     fun buttonEnabled(text: String, textSize: Float){
         this.text = text
         this.textSize = textSize
-        this.background = context.getDrawable(R.drawable.bg_button)
-        postInvalidate()
-    }
-
-    fun buttonDisabled(text: String){
-        this.text = text
-        this.textSize = textSize
-        this.background = context.getDrawable(R.drawable.bg_button)
+        this.background = AppCompatResources.getDrawable(context, R.drawable.bg_button)
         postInvalidate()
     }
 
